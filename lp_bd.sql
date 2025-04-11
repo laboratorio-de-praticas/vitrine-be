04c0527e773f28ce154c264aae1dbba548ef54a6
RenatoHioji <okamoto.hioji@gmail.com>
2025-04-11 17:25:03 -0300

    fix: ajustando sql

--- lp_bd.sql
+++ lp_bd.sql
@@ -128,3 +128,40 @@ CREATE TABLE "Avaliacao" (
   data_criacao TIMESTAMP DEFAULT now(),
   data_alteracao TIMESTAMP DEFAULT now()
 );
+
+
+INSERT INTO "Evento" (id_evento, nome_evento, tipo_evento, descricao_evento, status_evento, curso_semestre, data_alteracao, data_criacao, data_inicio, data_fim)
+VALUES
+(1, 'Representante de Classe', 'Interno', 'Evento para alunos DSM5', 'Ativo', 'DSM5', NOW(), NOW(), '2025-11-04', '2025-12-04'),
+(2, 'Representante de Classe', 'Interno', 'Evento para alunos GE1', 'Ativo', 'GE1', NOW(), NOW(), '2025-11-04', '2025-12-04'),
+(3, 'FATEC HUB', 'Externo', 'Evento da Fatec Hub', 'Ativo', null, NOW(), NOW(), '2025-11-04', '2025-12-04');
+
+INSERT INTO "Usuario" (id, nome, data_alteracao, data_criacao, senha, email_institucional, tipo_usuario, status_usuario)
+VALUES
+(1, 'Renato Hioji', NOW(), NOW(), '$2b$10$KRPrIGoPkM5RbtRrWdx.Au4frTROUrj9rdp6p8VPgLY2DG1mE72E.', 'renato_odake@fatec.sp.gov.br', 'Interno', 'Ativo'),
+(2, 'Saulo de Freitas', NOW(), NOW(), '$2b$10$KRPrIGoPkM5RbtRrWdx.Au4frTROUrj9rdp6p8VPgLY2DG1mE72E.', 'saulo@fatec.sp.gov.br', 'Interno', 'Ativo'),
+(3, 'Letícia Imasato', NOW(), NOW(), '$2b$10$KRPrIGoPkM5RbtRrWdx.Au4frTROUrj9rdp6p8VPgLY2DG1mE72E.', 'let_imasato@fatec.sp.gov.br', 'Interno', 'Ativo'),
+(4, 'Gabriel Rodriguez', NOW(), NOW(), '$2b$10$KRPrIGoPkM5RbtRrWdx.Au4frTROUrj9rdp6p8VPgLY2DG1mE72E.', 'gabriel_rod@fatec.sp.gov.br', 'Interno', 'Ativo'),
+(5, 'Administrador', NOW(), NOW(), '$2b$10$KRPrIGoPkM5RbtRrWdx.Au4frTROUrj9rdp6p8VPgLY2DG1mE72E.', 'admin@fatec.sp.gov.br', 'Admin', 'Ativo');;
+
+INSERT INTO "Aluno" (id_aluno, foto_url, data_criacao, data_alteracao, data_matricula, curso_semestre, fk_id_usuario)
+VALUES
+(1, '/candi4.jpg', NOW(), NOW(), '2025-01-15', 'DSM5', 1),
+(2, '/candi2.jpg', NOW(), NOW(), '2025-01-15', 'DSM5', 2),
+(3, '/candi1.jpg', NOW(), NOW(), '2025-01-15', 'DSM5', 3),
+(4, '/candi3.jpg', NOW(), NOW(), '2025-01-15', 'DSM5', 4);
+
+INSERT INTO "Representante" (id_representante, data_alteracao, data_criacao, representantesituacao, qrcode, fk_id_evento, fk_id_aluno)
+VALUES
+(1, NOW(), NOW(), 'Ativo', 'http://3.82.243.65:3000/votacao/interna/confirmacao/1/1', 1, 1),
+(2, NOW(), NOW(), 'Ativo', 'http://3.82.243.65:3000/votacao/interna/confirmacao/2/1', 1, 2),
+(3, NOW(), NOW(), 'Ativo', 'http://3.82.243.65:3000/votacao/interna/confirmacao/3/1', 1, 3),
+(4, NOW(), NOW(), 'Ativo', 'http://3.82.243.65:3000/votacao/interna/confirmacao/4/1', 1, 4);
+
+INSERT INTO "Projeto" (id_projeto, fk_id_aluno, titulo, descricao, qrcode, foto_url) VALUES 
+(1, 2, 'Tagarela', 'Projeto para auxílio de crianças com espectro autista.', 'http://3.82.243.65:3000/votacao/publica/confirmacao/avaliador/classificacao/id_projeto/id_evento/1/1', './sunside.png'),
+(2, 1,'EcoVision', 'Projeto para auxílio nos cuidados de plantas ornamentais.', 'http://3.82.243.65:3000/votacao/publica/confirmacao/convidado/id_projeto/id_evento/2/1', './ecovision.png');
+
+INSERT INTO "Visitante" VALUES (1, 'Renato Odake', '13123451234', '1234', NOW(), NOW());
+
+INSERT INTO "Avaliador" VALUES(1, 5, 'Hioji Okamoto', '13123451234', NOW(), NOW());
